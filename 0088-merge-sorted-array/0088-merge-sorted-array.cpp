5b635d5b1cb42cb8a564f47faaf93992e578c171
Menna Halim <menna20191701218@cis.asu.edu.eg>
2024-06-24 22:00:40 +0300

    Time: 0 ms (100.00%), Space: 10.9 MB (71.88%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -2,10 +2,20 @@ class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
 
-        int j = m;
-       for (int i =0; i <n;){
-           nums1[j++] = nums2[i++];
-       }
-        sort(nums1.begin(), nums1.end()); 
+        int p1 = m-1, p2 = n-1, i = n + m - 1;
+        while(p2 >= 0 && p1 >= 0){
+            if(nums1[p1] >= nums2[p2]){
+                nums1[i--] = nums1[p1--];
+            }
+            else{
+                nums1[i--] = nums2[p2--];
+            }
+        }
+        
+        if (p2 >= 0){
+            for (; i >= 0; ){
+                nums1[i--] = nums2[p2--];
+            }
+        }
     }
 };
